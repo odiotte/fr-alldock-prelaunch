e6d9d8ba03ce5d4e7505e0af3435dde45b6d6f9d
Olivier Diotte <olivier.diotte@gmail.com>
2015-03-03 14:24:23 -0500

    Initial commit FR

---
+++ app/models/user.rb
@@ -0,0 +1,53 @@
+class User < ActiveRecord::Base
+    belongs_to :referrer, :class_name => "User", :foreign_key => "referrer_id"
+    has_many :referrals, :class_name => "User", :foreign_key => "referrer_id"
+    
+    attr_accessible :email
+
+    validates :email, :uniqueness => true, :format => { :with => /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i, :message => "Invalid email format." }
+    validates :referral_code, :uniqueness => true
+
+    before_create :create_referral_code
+    after_create :send_welcome_email
+
+    REFERRAL_STEPS = [
+        {
+            'count' => 5,
+            "html" => "$5",
+            "class" => "two"
+        },
+        {
+            'count' => 10,
+            "html" => "$10",
+            "class" => "three"
+        },
+        {
+            'count' => 15,
+            "html" => "$15",
+            "class" => "four"
+        },
+        {
+            'count' => 20,
+            "html" => "$20",
+            "class" => "five"
+        }
+    ]
+
+    private
+
+    def create_referral_code
+        referral_code = SecureRandom.hex(5)
+        @collision = User.find_by_referral_code(referral_code)
+
+        while !@collision.nil?
+            referral_code = SecureRandom.hex(5)
+            @collision = User.find_by_referral_code(referral_code)
+        end
+
+        self.referral_code = referral_code
+    end
+
+    def send_welcome_email
+        UserMailer.delay.signup_email(self)
+    end
+end
