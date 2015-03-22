402b181b5d386afab7191f1e117e86d41384a7e4
Olivier Diotte <olivier.diotte@gmail.com>
2015-03-21 22:21:29 -0400

    Final Ended

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,6 @@ module Prelaunchr
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.ended = false
+    config.ended = true
   end
 end
